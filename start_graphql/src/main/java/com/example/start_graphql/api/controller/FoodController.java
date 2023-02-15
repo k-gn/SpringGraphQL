a145c3677564da1f9d665c30c317597c8ec3a06a
k-gn <rlarbska97@gmail.com>
2023-02-15 17:24:13 +0900

    feat: add controller

---
+++ start_graphql/src/main/java/com/example/start_graphql/api/controller/FoodController.java
@@ -0,0 +1,43 @@
+package com.example.start_graphql.api.controller;
+
+import java.util.List;
+
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+
+import com.example.start_graphql.api.domain.Food;
+import com.example.start_graphql.api.service.FoodService;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+public class FoodController {
+
+	private final FoodService foodService;
+
+	/**
+	 *  @MutationMapping은 @PostMapping과 같은 어노테이션으로 graphql에 Mutation에 사용됩니다.
+	 *  graphql은 endpoint과 하나이므로 @MutationMapping 어노테이션만 지정해 주고 다른 설정은 필요 없습니다.
+	 */
+	@MutationMapping
+	public Food save(String name) {
+		return foodService.save(name);
+	}
+
+	/**
+	 * @QueryMapping도 @GetMapping과 같은 어노테이션 입니다.
+	 * 말고도 @SubscriptionMapping이 있습니다.
+	 */
+	@QueryMapping
+	public Food getFood(@Argument String name) {
+		return foodService.getFood(name);
+	}
+
+	@QueryMapping
+	public List<Food> getFoods() {
+		return foodService.getFoods();
+	}
+}
