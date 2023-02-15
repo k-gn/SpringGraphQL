ea4c7a72c0c61bb481174e1c317a56ae6baa9019
k-gn <rlarbska97@gmail.com>
2023-02-15 17:24:07 +0900

    feat: add service

---
+++ start_graphql/src/main/java/com/example/start_graphql/api/service/FoodService.java
@@ -0,0 +1,33 @@
+package com.example.start_graphql.api.service;
+
+import java.util.List;
+
+import javax.persistence.EntityNotFoundException;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.start_graphql.api.domain.Food;
+import com.example.start_graphql.api.domain.FoodRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class FoodService {
+
+	private final FoodRepository foodRepository;
+
+	public Food save(String name) {
+		return foodRepository.save(Food.from(name));
+	}
+
+	public Food getFood(String name) {
+		return foodRepository.findByName(name).orElseThrow(EntityNotFoundException::new);
+	}
+
+	public List<Food> getFoods() {
+		return foodRepository.findAll();
+	}
+}
