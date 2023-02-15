c6ddc4704f7a32564033752c7d9b56195e17ae60
k-gn <rlarbska97@gmail.com>
2023-02-15 17:23:45 +0900

    chore: project init

---
+++ start_graphql/src/main/java/com/example/start_graphql/StartGraphqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.start_graphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartGraphqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartGraphqlApplication.class, args);
+	}
+
+}
